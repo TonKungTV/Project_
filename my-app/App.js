1ea38ed291ff98b3344f8b3e2262f9bcf2c95775
Aomsuchan <suchananssrithep@gmail.com>
2025-07-31 04:02:06 +0700

    Add files via upload

--- my-app/App.js
+++ my-app/App.js
@@ -1,9 +1,8 @@
-// App.js
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import HomeScreen from './screens/HomeScreen'; // 🔹 เพิ่มตรงนี้
 import MedicationListScreen from './screens/MedicationListScreen';
 import AddMedicationScreen from './screens/AddMedicationScreen';
 // import SettingsScreen from './screens/SettingsScreen';
@@ -13,7 +12,8 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="MedicationList">
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ title: 'หน้าหลัก' }} />
         <Stack.Screen name="MedicationList" component={MedicationListScreen} options={{ title: 'รายการยา' }} />
         <Stack.Screen name="AddMedication" component={AddMedicationScreen} options={{ title: 'เพิ่มยา' }} />
         {/* <Stack.Screen name="Settings" component={SettingsScreen} options={{ title: 'ตั้งค่าผู้ใช้' }} /> */}
